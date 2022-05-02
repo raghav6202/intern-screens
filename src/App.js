0e43f2ebaa53895c245afcf2f3f336f84de74cb4
Raghav Chandra V S <86127392+raghav6202@users.noreply.github.com>
2022-05-02 13:49:16 +0530

    clone: from intern-screens

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./styles.css";
-
+import { Screen1 } from "./screens/screen1/screen1.jsx";
+import { Screen2 } from "./screens/screen2/screen2.jsx";
+import { Screen3 } from "./screens/screen3/screen3.jsx";
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Screen1 />
+      <Screen2 />
+      <Screen3 />
     </div>
   );
 }
